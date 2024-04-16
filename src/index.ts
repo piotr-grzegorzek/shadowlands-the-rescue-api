807a166ec058f572d8869397240367a56b62850f
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-04-16 07:30:35 +0200

    chore: prettier-eslint
    https://marketplace.visualstudio.com/items?itemName=rvest.vs-code-prettier-eslint

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
