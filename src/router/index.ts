4224ab8831d0d218bb2e913489e94bd704b7bd5c
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-04-19 09:13:51 +0200

    feat: get users with auth middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { isAuthenticated, login, register } from "../controllers/authentication";
+import { isAuthenticatedMiddleware } from "../middleware";
+import { getAllUsers } from "../controllers/users";
 
 const router = express.Router();
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
 router.get("/auth", isAuthenticated);
+router.get("/users", isAuthenticatedMiddleware, getAllUsers);
 
 export default router;
